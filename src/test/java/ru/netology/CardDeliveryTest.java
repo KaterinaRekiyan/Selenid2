eb52e5b3c1b48cdfc03f05cd057052ca99874d6b
Ekaterina <Katerina.rekiyan@yandex.ru>
2023-01-26 18:06:25 +0300

    build

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,6 +1,7 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -12,13 +13,16 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
     private String generateDate(int addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
     @Test
     public void shouldBeSuccesfullyCompleted() {
-        open ("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Москва");
         String currentDate = generateDate(3, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
